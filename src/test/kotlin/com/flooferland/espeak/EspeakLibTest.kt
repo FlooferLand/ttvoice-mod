fb76410314d1a61f40e5e737d43ddb650d989e2c
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-07-25 01:28:18 +0300

    Improve tests

--- src/test/kotlin/com/flooferland/espeak/EspeakLibTest.kt
+++ src/test/kotlin/com/flooferland/espeak/EspeakLibTest.kt
@@ -1,14 +1,42 @@
 package com.flooferland.espeak
 
+import com.flooferland.ttvoice.speech.EspeakSpeaker
 import io.kotest.core.spec.style.FunSpec
+import org.junit.jupiter.api.Assertions.*
 
 class EspeakLibTest : FunSpec({
-    test("initialize") {
-        Espeak.initialize()
+    val expectedChunks = 11
+
+    test("initial") {
+        val result = Espeak.initialize()
+        when (result) {
+            is Espeak.Result.Success<Int> -> {
+                assertTrue(result.data == EspeakSpeaker.INPUT_SAMPLERATE, "${result.data} == ${EspeakSpeaker.INPUT_SAMPLERATE}")
+            }
+            is Espeak.Result.Failure -> {
+                fail("Could not initialize")
+            }
+            else -> {}
+        }
+        Espeak.terminate()
     }
 
     test("speak") {
-        Espeak.initialize()
-        Espeak.synth("Hello World!")
+        var chunkCount = 0
+        Espeak.initialize(Espeak.AudioOutput.Retrieval, 300)
+        Espeak.setSynthCallback { wav, numberOfSamples, events ->
+            chunkCount += 1
+            if (chunkCount != expectedChunks) {
+                println("CHUNK[$chunkCount]:\t(wav[1]=${wav?.getShortArray(0, numberOfSamples)[0]},\tnumberOfSamples=$numberOfSamples,\tevents=$events)")
+            } else {
+                println("CHUNK: end")
+            }
+            return@setSynthCallback 0
+        }
+        Espeak.synth("Hello guys and welcome back to Minecraft!")
+        assertTrue(chunkCount > 0, "No chunks")
+        assertEquals(chunkCount, expectedChunks, "Wrong number of chunks")
+        println("Total: $chunkCount chunks")
+        Espeak.terminate()
     }
 })
