305c2dd155e62d7a4245f9d90814bed32cd2db6c
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-10 10:41:38 +0300

    Add tabs to config screen

--- src/client/kotlin/com/flooferland/ttvoice/screen/ConfigScreen.kt
+++ src/client/kotlin/com/flooferland/ttvoice/screen/ConfigScreen.kt
@@ -21,7 +21,6 @@ import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import net.minecraft.util.math.ColorHelper
 import java.util.Optional
-import kotlin.jvm.optionals.getOrElse
 import kotlin.jvm.optionals.getOrNull
 import kotlin.reflect.KMutableProperty1
 import kotlin.reflect.KProperty1
@@ -30,8 +29,12 @@ import kotlin.reflect.full.memberProperties
 private val title = Text.translatable("config.${MOD_ID}.title")
 private val WHITE_COLOR: Int = ColorHelper.Argb.getArgb(255, 255, 255, 255)
 
+typealias TabId = String
+
 class ConfigScreen(val parent: Screen) : Screen(title) {
-    val widgets = mutableListOf<ClickableWidget>()
+    val widgets = mutableMapOf<TabId, MutableList<ClickableWidget>>()
+    var categoryContext: TabId = ""
+    var selectedCategory: TabId = ""
     lateinit var saveButton: ButtonWidget
     lateinit var noticeLabel: TextWidget
     lateinit var currentConfig: TextToVoiceConfig
@@ -134,15 +137,19 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
     }
 
     fun autoSettings() {
-        for (widget in widgets) {
-            remove(widget)
+        for (array in widgets.values) {
+            for (widget in array) {
+                remove(widget)
+            }
         }
         widgets.clear()
+        selectedCategory = TextToVoiceConfig::class.memberProperties.first().name
 
-        val offset = MutVector2Int(10, 40 /* Space for the title */)
         for (categoryProp in TextToVoiceConfig::class.memberProperties) {
             val categoryName = categoryProp.name
             var categoryValue = (categoryProp.get(currentConfig) ?: continue)
+            categoryContext = categoryName
+            widgets[categoryContext] = mutableListOf()
 
             // Special redirect for voice config, since the type changes
             if (categoryValue is VoiceConfig) {
@@ -152,6 +159,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
             }
 
             // Loop and populate all the props
+            val offset = MutVector2Int(10, 40 /* Space for the title */)
             for ((i, field) in categoryValue::class.memberProperties.withIndex()) {
                 val fieldName = field.name
                 val fieldInitialValue = (field as KProperty1<Any, *>).get(categoryValue)
@@ -179,7 +187,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                                 .size(size.x, size.y)
                                 .build()
                             selectDeviceButton.active = ModState.config.general.routeThroughDevice
-                            addWidget(selectDeviceButton)
+                            addConfigWidget(selectDeviceButton)
                             return@run
                         }
 
@@ -199,7 +207,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                                     }*/
                                     setSetting(field, categoryValue, v)
                                 }
-                            addWidget(b)
+                            addConfigWidget(b)
                             return@run
                         }
 
@@ -229,7 +237,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                                     SpeechUtil.updateVoice(currentConfig.voice.espeak.name)
                                     SpeechUtil.playTest()
                                 }
-                            addWidget(b)
+                            addConfigWidget(b)
                             return@run
                         }
                     }
@@ -249,7 +257,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                                     }
                                 }
                             }
-                            addWidget(thing)
+                            addConfigWidget(thing)
                         }
 
                         String::class -> {
@@ -258,7 +266,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                             val label = TextWidget(offset.x + 5, offset.y, 200, 15, labelText, textRenderer)
                                 .alignLeft()
                             addDrawableChild(label)
-                            addWidget(label)
+                            addConfigWidget(label)
 
                             val tooltip = Text.translatableWithFallback("${translationString}.tooltip", labelText.string)
                             val thing = TextFieldWidget(textRenderer, position.x+1, position.y+15, size.x, 18, Text.of(fieldInitialValue as String))
@@ -268,7 +276,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                             thing.setChangedListener { v ->
                                 setSetting(field, categoryValue, v)
                             }
-                            addWidget(thing)
+                            addConfigWidget(thing)
                         }
 
                         else -> {
@@ -281,10 +289,51 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
                 offset.y += (size.y + pad)
             }
         }
+
+        // Only showing widgets of the current category
+        for ((id, widgets) in widgets) {
+            for (widget in widgets) {
+                widget.visible = (id == selectedCategory)
+            }
+        }
+
+        // Add category tab buttons
+        val xPad = 8
+        var xOffset = 120 // Offset for the title
+        val targetButtonSize = Vector2Int(80, 18)
+        val minWidth = xOffset + (targetButtonSize.x + xPad) * widgets.keys.size
+        var size = targetButtonSize
+        if (width < minWidth) {
+            size = Vector2Int(targetButtonSize.x - ((minWidth - width) * 0.25).toInt(), targetButtonSize.y)
+        }
+        val tabButtons = mutableListOf<ClickableWidget>()
+        for (id in widgets.keys) {
+            val button = ButtonWidget.builder(Text.translatable("config.$MOD_ID.$id"))
+                { b ->
+                    selectedCategory = id
+
+                    for (button in tabButtons) {
+                        button.setAlpha(if (button == b) 1.0f else 0.6f)
+                    }
+
+                    for ((id, widgets) in widgets) {
+                        for (widget in widgets) {
+                            widget.visible = (id == selectedCategory)
+                        }
+                    }
+                }
+                .position(xOffset, 5)
+                .size(size.x, size.y)
+                .build()
+            button.setAlpha(if (id == selectedCategory) 1.0f else 0.6f)
+            addDrawableChild(button)
+            tabButtons.add(button)
+            xOffset += size.x + xPad
+        }
     }
 
-    fun addWidget(widget: ClickableWidget) {
-        widgets.add(widget)
+    fun addConfigWidget(widget: ClickableWidget) {
+        widgets[categoryContext]?.add(widget)
         addDrawableChild(widget)
     }
 
@@ -309,7 +358,7 @@ class ConfigScreen(val parent: Screen) : Screen(title) {
     }
 
     override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
-        context.drawCenteredTextWithShadow(textRenderer, com.flooferland.ttvoice.screen.title, width / 2, 10, WHITE_COLOR)
+        context.drawTextWithShadow(textRenderer, com.flooferland.ttvoice.screen.title, 10, 10, WHITE_COLOR)
         renderBackground(context)
         context.fillGradient(
             0, 0, width, 30,
